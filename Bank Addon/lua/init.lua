13b9ee56ac8d0d9b4256a9b584df13352829b7f8
ToastedTurt <131556465+ToastedTurt@users.noreply.github.com>
2023-04-22 18:37:22 -0400

    ...

--- Bank Addon/lua/init.lua
+++ Bank Addon/lua/init.lua
@@ -1,7 +1,10 @@
 if SERVER then
     include("banking_system.lua")
+    include("atm_entity.lua")
 end
 
 if CLIENT then
     include("banking_system.lua")
+    include("atm.lua")
+    include("atm_entity.lua")
 end
